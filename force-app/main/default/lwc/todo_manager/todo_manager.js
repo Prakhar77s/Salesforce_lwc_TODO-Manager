d50a979dd17640848e8970cd41baf18605ad8e02
Prakhar Singh <prakhars76@gmail.com>
2022-05-26 16:54:17 +0530

    added basic layout and scripts

---
+++ force-app/main/default/lwc/todo_manager/todo_manager.js
@@ -0,0 +1,56 @@
+import { LightningElement } from 'lwc';
+
+export default class Todo_manager extends LightningElement {
+    time="7pm";
+    greeting="good evening"
+
+    connectedCallback() {
+        //get current time
+        this.getTime();
+    
+     
+        //get time periodically after every minute
+        setInterval(() => {
+          this.getTime();
+        }, 1000 * 60);
+      }
+    
+
+    getTime() {
+        const date = new Date(); /* creating object of Date class */
+        const hour = date.getHours();
+        const min = date.getMinutes();
+
+        this.time = `${this.getHour(hour)}:${this.getDoubleDigit(
+            min
+          )} ${this.getMidDay(hour)}`;
+          //get greeting (mornig/afternoon/evening/)
+          this.setGreeting(hour);
+      }
+
+       //Convert 24 hours format to 12 hours format
+  getHour(hour) {
+    return hour == 0 ? 12 : hour > 12 ? hour - 12 : hour;
+  }
+
+  //convert single digit to double digit
+  getDoubleDigit(digit) {
+    return digit < 10 ? "0" + digit : digit;
+  }
+
+  //return AM or PM based on current hour
+  getMidDay(hour) {
+    return hour >= 12 ? "PM" : "AM";
+  }
+
+      setGreeting(hour) {
+        if (hour < 12) {
+          this.greeting = "Good Morning";
+        } else if (hour >= 12 && hour < 17) {
+          this.greeting = "Good Afternoon";
+        } else {
+          this.greeting = "Good Evening";
+        }
+      }
+
+}   
